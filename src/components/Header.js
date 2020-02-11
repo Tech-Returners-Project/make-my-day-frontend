ddea03d8595fca83987358565278974e12f7c59f
Alicjamp <alicja.parker@outlook.com>
2020-02-11 22:26:08 +0000

    started setting up questionnaire

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../components/Header.css'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title"> Make my Day!</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
