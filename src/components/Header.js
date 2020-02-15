7bfd140163bc0b3af3e80fe3fe30ec8e1e826b2b
Nichola Evans <nichola867@hotmail.com>
2020-02-15 11:39:48 +0000

    Background and logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
 import React from "react";
 import '../components/Header.css'
+//import '../MakeMyDayLogo.png'
 
 class Header extends React.Component {
     render() {
         return (
-            <div>
-                <h1 className="title"> Make my Day!</h1>
+            <div className = "container">
+
+                <div className = "row" >
+                    <div className = "col-12">
+                        <img className = "logo" 
+                        //src = "logo192.png" 
+                        //src = "..src/MakeMyDayLogo.png" 
+                        src = "MakeMyDayLogoCrop.png"
+                        alt = "" 
+                        />
+                    </div>
+                </div>
+
             </div>
         )
     }
 }
 
 export default Header;
+
+
+
+//<h1 className="title"> Make my Day!</h1>
