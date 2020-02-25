d2c358d8e0f1ef9c2bca430a319b23b2e6c09813
Alicjamp <alicja.parker@outlook.com>
2020-02-25 15:31:22 +0000

    Updates

---
+++ precache-manifest.1afb8181ddc08e88c0060ead80f425b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc45e2f3bc674a6953f1bf0982800ed",
+    "url": "/make-my-day-frontend/index.html"
+  },
+  {
+    "revision": "1a594b02c32eed560eac",
+    "url": "/make-my-day-frontend/static/css/main.909eaae4.chunk.css"
+  },
+  {
+    "revision": "90f56e739794d1c2dc07",
+    "url": "/make-my-day-frontend/static/js/2.fd125da7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/make-my-day-frontend/static/js/2.fd125da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a594b02c32eed560eac",
+    "url": "/make-my-day-frontend/static/js/main.24e17b58.chunk.js"
+  },
+  {
+    "revision": "185fd9a8635c6772f680",
+    "url": "/make-my-day-frontend/static/js/runtime-main.07de80d5.js"
+  },
+  {
+    "revision": "8e5e0a804ed70f001aea18e4de2b71e0",
+    "url": "/make-my-day-frontend/static/media/cinema.8e5e0a80.png"
+  }
+]);
